728db903a65a216eabd0e1e6fd9882fb3098c82c
kirun9 <kpawelek99@wp.pl>
2019-05-08 19:40:23 +0200

    Added Option to perform click on tabButton from code

--- Momiji V5/Controls/TabButton.cs
+++ Momiji V5/Controls/TabButton.cs
@@ -29,6 +29,12 @@ namespace Momiji.Bot.V5.Core.Controls
 			base.OnPaint(e);
 		}
 
+		public void PerformClick()
+		{
+			MouseEventArgs args = new MouseEventArgs(MouseButtons.None, 0, 0, 0, 0);
+			TabButton_MouseClick(this, args);
+		}
+
 		private void TabButton_MouseClick(Object sender, MouseEventArgs e)
 		{
 			var controls = Parent.Controls;
