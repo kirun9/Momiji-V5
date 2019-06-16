5c8fea4f8359e6e323488f20b1fb43e1fe9a3fd5
kirun9 <kpawelek99@wp.pl>
2019-06-16 20:56:21 +0200

    Changes in date display Now Full date is displayed as tooltip when mouse is over date

--- Momiji V5/InternalServer/LogMessage.cs
+++ Momiji V5/InternalServer/LogMessage.cs
@@ -9,10 +9,10 @@ namespace Momiji.Bot.V5.Core.InternalServer
 	public class LogMessage
 	{
 		public string Message { get; private set; }
-		public string Date { get; private set; }
+		public DateTime Date { get; private set; }
 		public string ModuleName { get; private set; }
 		public ConsoleMessageType MessageType { get; private set; }
-		public LogMessage(string date, string moduleName, string message, ConsoleMessageType messageType = ConsoleMessageType.Info)
+		public LogMessage(DateTime date, string moduleName, string message, ConsoleMessageType messageType = ConsoleMessageType.Info)
 		{
 			Message = message;
 			Date = date;
@@ -29,12 +29,12 @@ namespace Momiji.Bot.V5.Core.InternalServer
 			string output = "";
 			if (MessageType == ConsoleMessageType.Warning || MessageType == ConsoleMessageType.Error)
 			{
-				output += $"<tr><td class=\"row1{(colors.Item1 != "" ? " " + colors.Item1 : "")}\">{Date}</td><td class=\"row2{(colors.Item2 != "" ? " " + colors.Item2 : "")}\">{ModuleName}</td><td class=\"row3{(colors.Item3 != "" ? " " + colors.Item3 : "")}\">Caught Exception:</td></tr>\n";
+				output += $"<tr><td class=\"row1{(colors.Item1 != "" ? " " + colors.Item1 : "")}\"><abbr title=\"{Date.ToString("MMM dd, YYYY")}\">{Date.ToString("HH:mm:ss")}</abbr></td><td class=\"row2{(colors.Item2 != "" ? " " + colors.Item2 : "")}\">{ModuleName}</td><td class=\"row3{(colors.Item3 != "" ? " " + colors.Item3 : "")}\">Caught Exception:</td></tr>\n";
 				output += $"<tr><td class=\"multirow{(colors.Item3 != "" ? " " + colors.Item3 : "")}\" colspan=3>{Message}</td></tr>\n";
 			}
 			else
 			{
-				output += $"<tr><td class=\"row1{(colors.Item1 != "" ? " " + colors.Item1 : "")}\">{Date}</td><td class=\"row2{(colors.Item2 != "" ? " " + colors.Item2 : "")}\">{ModuleName}</td><td class=\"row3{(colors.Item3 != "" ? " " + colors.Item3 : "")}\">{Message}</td></tr>\n";
+				output += $"<tr><td class=\"row1{(colors.Item1 != "" ? " " + colors.Item1 : "")}\"><abbr title=\"{Date.ToString("MMM dd, YYYY")}\">{Date.ToString("HH:mm:ss")}</abbr></td><td class=\"row2{(colors.Item2 != "" ? " " + colors.Item2 : "")}\">{ModuleName}</td><td class=\"row3{(colors.Item3 != "" ? " " + colors.Item3 : "")}\">{Message}</td></tr>\n";
 			}
 			return output;
 		}
